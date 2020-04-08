7f68b5b5854d7853a4f8e452493653916b866d7c
Nick Panin <panin@adwz.ru>
2020-04-08 19:53:27 +0300

    fix for equal dates

--- app/src/main/java/com/example/appdiploma/Note.java
+++ app/src/main/java/com/example/appdiploma/Note.java
@@ -113,6 +113,7 @@ public class Note implements Serializable {
         today.set(Calendar.HOUR_OF_DAY, 0);
         today.set(Calendar.MINUTE, 0);
         today.set(Calendar.SECOND, 0);
+        today.set(Calendar.MILLISECOND, 0);
 
         return this.getDate().compareTo(today);
     }
