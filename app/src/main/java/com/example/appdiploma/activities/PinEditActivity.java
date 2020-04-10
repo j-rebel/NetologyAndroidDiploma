374287c058e5d20cf6ceab6c394b7b28285c523a
Nick Panin <panin@adwz.ru>
2020-04-11 00:44:20 +0300

    deleted excess onclick for pin edit screen

--- app/src/main/java/com/example/appdiploma/activities/PinEditActivity.java
+++ app/src/main/java/com/example/appdiploma/activities/PinEditActivity.java
@@ -33,25 +33,6 @@ public class PinEditActivity extends ToolbarActivity {
     public void initView() {
         saveButton = findViewById(R.id.button_save);
         pinInput = findViewById(R.id.pinInput);
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String newPin = pinInput.getText().toString();
-
-                if (newPin.length() == 4) {
-                    App.getInstance().getKeystore().saveNew(newPin);
-                    App.getInstance().setNotFirstTime();
-                    Toast.makeText(App.getInstance().getContext(), getString(R.string.pin_edit_ok), Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(PinEditActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    pinInput.setText("");
-                    Toast.makeText(App.getInstance().getContext(), getString(R.string.pin_edit_error), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
     }
 
     public void initToolbar() {
